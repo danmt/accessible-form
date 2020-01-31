ecc9af1701c2ce8fec3d4fe4e6d3e9d69c5cdaf7
Daniel Marin <danielarturomt@gmail.com>
2020-01-31 20:09:33 -0300

    final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -7,16 +7,28 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('errorsSection', { static: true }) errorsSection: ElementRef;
   submitted = false;
   form = this.fb.group({
     firstName: ['', { validators: [Validators.required] }],
     lastName: ['', { validators: [Validators.required] }],
     email: ['', { validators: [Validators.required, Validators.email] }]
   });
+  errors = null;
 
   constructor(private fb: FormBuilder) {}
 
   onSubmit() {
     this.submitted = true;
+    if (this.form.invalid) {
+      this.errors = {
+        firstName: this.form.get('firstName').errors,
+        lastName: this.form.get('lastName').errors,
+        email: this.form.get('email').errors
+      };
+      this.errorsSection.nativeElement.focus();
+    } else {
+      this.errors = null;
+    }
   }
 }
